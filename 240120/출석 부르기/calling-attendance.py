e82b9bb8a61cf309d8b418ebe0aa2e2251dc020f
12230563 <143669470+12230563@users.noreply.github.com>
2024-01-20 15:00:14 +0900

    Title: 출석 부르기, Time: 205ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240120/출석 부르기/calling-attendance.py
@@ -0,0 +1,10 @@
+Attendance_number = int(input())
+
+if Attendance_number == 1:
+    print("John")
+elif Attendance_number == 2:
+    print("Tom")
+elif Attendance_number == 3:
+    print("Paul")
+else:
+    print("Vacancy")
