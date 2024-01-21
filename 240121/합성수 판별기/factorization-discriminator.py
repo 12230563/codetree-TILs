e267b6194b9ac69f5b730fdfa70ad9da68777cb0
12230563 <143669470+12230563@users.noreply.github.com>
2024-01-21 17:33:00 +0900

    Title: 합성수 판별기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240121/합성수 판별기/factorization-discriminator.py
@@ -0,0 +1,14 @@
+def isprime(n):
+    i = 2
+    while i ** 2 <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True    
+
+n = int(input())
+
+if isprime:
+    print("C")
+else:
+    print("N")
