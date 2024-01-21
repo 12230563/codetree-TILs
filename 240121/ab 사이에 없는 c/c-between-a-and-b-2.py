d20aa131b062ffd2ec7fd405e8fca7ca424d5b8b
12230563 <143669470+12230563@users.noreply.github.com>
2024-01-21 17:42:05 +0900

    Title: ab 사이에 없는 c, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240121/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240121/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -1,12 +1,18 @@
 a, b, c = list(map(int,input().split()))
 
 
+cout = 0
+
 def ismultiple(n1,n2,n3):
     for i in range(n1,n2+1):
         if i % n3 == 0:
-            return False
+            cout += 1            
         else:
-            return True
+            pass
+    if cout == 0:
+        return True
+    else:
+        return False            
 
 if ismultiple(a,b,c):
     print("YES")
