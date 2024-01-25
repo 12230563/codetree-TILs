c3c7dafabfe73c242b00aac29b30f1a5e0524c62
12230563 <143669470+12230563@users.noreply.github.com>
2024-01-25 13:50:45 +0900

    Title: 소수 판별기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/소수 판별기/prime-number-discriminator.cpp
+++ 240125/소수 판별기/prime-number-discriminator.cpp
@@ -1,35 +1,30 @@
 #include <iostream>
 using namespace std;
 
-bool isprime(int n){
-    if(n <= 2){
-        return false;
+bool isprime(int n) {
+    if (n <= 1) {
+        return false; 
     }
-    
-    for(int i = 2;i*i <= n;i++){
-        if(n % (i * i) == 0){
-            return false;
-            break;
-        }
-        else{
-            return true;
-        }
-    }
-}
 
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 int main() {
     int n;
 
     cin >> n;
 
-
-    if(isprime(n) == true){
+    if (isprime(n)) {
         cout << 'P';
-    }
-    else{
+    } else {
         cout << 'C';
     }
 
-
+    return 0;
 }
