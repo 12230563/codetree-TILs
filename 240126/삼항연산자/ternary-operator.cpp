092dde0f788475143a123b2176894fccb84e6400
12230563 <143669470+12230563@users.noreply.github.com>
2024-01-26 11:15:02 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240126/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int score;
+    string grade;
+
+    cin >> score;
+    grade = (score == 100) ? "pass" : "failure"; 
+    cout << grade;
+}
