532e9530a7ab208d831fcb2060d459511008e9e5
12230563 <143669470+12230563@users.noreply.github.com>
2024-01-27 16:04:09 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240127/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n ,k = 0;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> n;
+        if (n % 2 == 0) {
+            k++;
+        }
+    }
+    cout << k;
+}
