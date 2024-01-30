4124ebdf89740ba1d946b27f8a22ec813753031b
12230563 <143669470+12230563@users.noreply.github.com>
2024-01-30 15:21:29 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240130/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
+++ 240130/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -3,25 +3,28 @@ using namespace std;
 
 int main() {
     int arr[10][4], i, j, n, cnt = 0;
+    int sum = 0;
     cin >> n;
-
+    
     for (i = 0; i < n; i++) {
-        for (j = 0; j < 3; j++) {
+        for (j = 0; j < 4; j++) {
             cin >> arr[i][j];
         }
     }
 
     for (i = 0; i < n; i++) {
-        int sum = 0;
-        for (j = 0; j < 3; j++) {
+        sum = 0;
+
+        for (j = 0; j < 4; j++) {
             sum += arr[i][j];
         }
         if (sum >= 240) {
             cout << "pass" << '\n';
             cnt++;
         }
-        else
+        else {
             cout << "fail" << '\n';
+        }    
     }
     cout << cnt;
     
