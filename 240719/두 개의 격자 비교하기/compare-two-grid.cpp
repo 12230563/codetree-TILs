a5cd72e6781ea530fae2e960d1011649355b5021
12230563 <143669470+12230563@users.noreply.github.com>
2024-07-19 13:00:12 +0900

    Title: 두 개의 격자 비교하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240719/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a[10][10], b[10][10];
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> b[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] != b[i][j]) cout << 1 << ' ';
+            else cout << 0 << ' ';
+        }
+        cout << '\n';
+    }
+        
+
+
+}
