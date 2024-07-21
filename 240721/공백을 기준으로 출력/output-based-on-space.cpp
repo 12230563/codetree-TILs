d289029c57eef1a1358180d29ae6afc0018708c8
12230563 <143669470+12230563@users.noreply.github.com>
2024-07-21 14:45:11 +0900

    Title: 공백을 기준으로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/공백을 기준으로 출력/output-based-on-space.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+    string str1, str2;
+    getline(cin, str1);
+    getline(cin, str2);
+    for (char c : str1) {
+        if (c != ' ') cout << c;
+    }
+    for (char c : str2) {
+        if (c != ' ') cout << c;
+    }
+
+}
