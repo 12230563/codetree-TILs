b659699dccacc2c7c57dd1e65747f858e910bf5f
12230563 <143669470+12230563@users.noreply.github.com>
2024-07-28 01:46:51 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240728/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
+++ 240728/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.cpp
@@ -5,31 +5,30 @@ using namespace std;
 
 int main() {
     bool cond = true;
+    string s1, s2;
     vector<char> v1, v2;
 
-    for (char &i : v1) {
-        cin >> i;
-        if (i == '\n') break;
-    }
-    for (char &i : v2) {
-        cin >> i;
-        if (i == '\n') break;
-    }
+    getline(cin, s1);
+    getline(cin, s2);
+
+    v1.assign(s1.begin(), s1.end());
+    v2.assign(s2.begin(), s2.end());
+
     sort(v1.begin(), v1.end());
     sort(v2.begin(), v2.begin());
 
     if (v1.size() != v2.size()) {
-        cout << "No";
-        return 0;
+        cond = false;
     }
-
-    for (int i = 0; i < v1.size(); i++) {
-        if (v1[i] != v2[i]) {
-            cond = false;
-            break;
+    else {
+        for (int i = 0; i < v1.size(); i++) {
+            if (v1[i] != v2[i]) {
+                cond = false;
+                break;
+            }
         }
     }
-
+    
     if (cond) cout << "Yes";
     else cout << "No";
 }
